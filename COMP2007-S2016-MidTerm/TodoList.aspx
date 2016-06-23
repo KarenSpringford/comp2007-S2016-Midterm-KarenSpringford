16e68c0e13c7fce33e1f9f2c1076633c780693e4
KarenSpringford <jkspringford@gmail.com>
2016-06-23 09:50:28 -0400

    ToDo List with pageination

--- COMP2007-S2016-MidTerm/TodoList.aspx
+++ COMP2007-S2016-MidTerm/TodoList.aspx
@@ -1,5 +1,43 @@
 ﻿<%@ Page Title="Todo List" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TodoList.aspx.cs" Inherits="COMP2007_S2016_MidTerm.TodoList" %>
-
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-     
+
+        <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <h1>To Do List</h1>
+                <br />
+                <a href="TodoDetails.aspx" class="btn btn-success btn-sm"><i class="fa fa-plus fa-lg"></i> Add A To Do</a>
+                <div>
+                    <label for="PageSizeDropDownList">To Do's per Page: </label>
+                    <asp:DropDownList ID="PageSizeDropDownList" runat="server"
+                        AutoPostBack="true" CssClass="btn btn-default bt-sm dropdown-toggle"
+                        OnSelectedIndexChanged="PageSizeDropDownList_SelectedIndexChanged">
+                        <asp:ListItem Text="3" Value="3" />
+                        <asp:ListItem Text="5" Value="5" />
+                        <asp:ListItem Text="10" Value="10" />
+                        <asp:ListItem Text="All" Value="10000" />
+                    </asp:DropDownList>
+                </div>
+                <br />
+                <asp:GridView runat="server" ID="TodosGridView" AutoGenerateColumns="false" CssClass="table table-bordered table-striped table-hover" DataKeyNames="TodoID"
+                AllowPaging="true" PageSize="3" OnPageIndexChanging="TodosGridView_PageIndexChanging" AllowSorting="true" OnSorting="TodosGridView_Sorting"
+                OnRowDataBound="TodosGridView_RowDataBound" OnRowDeleting="TodosGridView_RowDeleting" PagerStyle-CssClass="pagination-ys">
+
+                <Columns>
+                        <asp:BoundField DataField="TodoID" HeaderText="To Do ID" Visible="true"/>
+                        <asp:BoundField DataField="TodoName" HeaderText="To Do Name" Visible="true"/>
+                        <asp:BoundField DataField="TodoNotes" HeaderText="Notes" Visible="true"/>
+                        <asp:BoundField DataField="Completed" HeaderText="Completed" Visible="true"/>
+                        <asp:CheckBoxField DataField="Completed" HeaderText="Completed" Visible="true" />
+
+                        <asp:HyperLinkField HeaderText="Edit" Text="<i class='fa fa-pencil-square-o fa-lg'></i> Edit" 
+                            NavigateUrl="~/TodoDetails.aspx.cs" ControlStyle-CssClass="btn btn-primary btn-sm" runat="server"
+                            DataNavigateUrlFields="TodoID" DataNavigateUrlFormatString="TodoDetails.aspx?TodoID={0}" />
+                        <asp:CommandField  HeaderText="Delete" DeleteText="<i class='fa fa-trash-o fa-lg'></i> Delete"
+                            ShowDeleteButton="true" ButtonType="Link" ControlStyle-CssClass="btn btn-danger btn-sm" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </div>     
 </asp:Content>
--- COMP2007-S2016-MidTerm/TodoList.aspx.cs
+++ COMP2007-S2016-MidTerm/TodoList.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//using statements that are requuired to connect the the EF DB
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+using COMP2007_S2016_MidTerm.Models;
+
 namespace COMP2007_S2016_MidTerm
 {
     public partial class TodoList : System.Web.UI.Page
@@ -12,8 +17,170 @@ namespace COMP2007_S2016_MidTerm
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            //if loading the page for the first time, populate the ToDo grid
+            if (!IsPostBack)
+            {
+                Session["SortColumn"] = "TodoID"; // default sort column
+                Session["SortDirection"] = "ASC";
+                // Get the Todo data
+                this.GetTodo();
+            }
+
+
         }
-        
+
+        /**
+         * <summary>
+         * This method gets the student data from the DB
+         * </summary>
+         * @method GetStudents
+         * @return (void)
+         * */
+         protected void GetTodo()
+        {
+            //connect to the EF
+            using (TodoConnection db = new TodoConnection())
+            {
+
+                string SortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+
+                //query the students table using EF and LINQ
+                var Todos = (from allTodos in db.Todos
+                                select allTodos);
+
+                //sends the data to a list
+                TodosGridView.DataSource = Todos.AsQueryable().OrderBy(SortString).ToList();
+                //binds the data to the grid
+                TodosGridView.DataBind();
+
+            }
+        }
+
+        /**
+         * <summary>
+         * This event handler allows pagination to occur for the Todo page
+         * </summary>
+         * 
+         * @method TodosGridView_PageIndexChanging
+         * @param {object} sender
+         * @param {GridViewPageEventArgs} e
+         * @returns {void}
+         */
+        protected void TodosGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // Set the new page number
+            TodosGridView.PageIndex = e.NewPageIndex;
+
+            // refresh the grid
+            this.GetTodo();
+        }
+
+        /**
+         * <summary>
+         * This event handler allows sorting to occur for the Todo page
+         * </summary>
+         * 
+         * @method TodosGridView_Sorting
+         * @param {object} sender
+         * @param {GridViewSortEventArgs} e
+         * @returns {void}
+         */
+        protected void TodosGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            // get the column to sorty by
+            Session["SortColumn"] = e.SortExpression;
+
+            // Refresh the Grid
+            this.GetTodo();
+
+            // toggle the direction
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "ASC" ? "DESC" : "ASC";
+
+        }
+
+        /**
+        * <summary>
+        * This event handler deletes a todo from the db using EF
+        * </summary>
+        * 
+        * @method TodosGridView_RowDeleting
+        * @param {object} sender
+        * @param {GridViewDeleteEventArgs} e
+        * @returns {void}
+        */
+        protected void TodosGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected TodoID using the Grid's DataKey collection
+            int TodoID = Convert.ToInt32(TodosGridView.DataKeys[selectedRow].Values["TodoID"]);
+
+            // use EF to find the selected todo in the DB and remove it
+            using (TodoConnection db = new TodoConnection())
+            {
+                // create object of the todo class and store the query string inside of it
+                Todo deletedTodo = (from todoRecords in db.Todos
+                                          where todoRecords.TodoID == TodoID
+                                          select todoRecords).FirstOrDefault();
+
+                // remove the selected todo from the db
+                db.Todos.Remove(deletedTodo);
+
+                // save my changes back to the database
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetTodo();
+            }
+        }
+
+        /**
+        * <summary>
+        * This event handler data binding a todo to the row from the db using EF
+        * </summary>
+        * 
+        * @method TodosGridView_RowDataBound
+        * @param {object} sender
+        * @param {GridViewRowEventArgs} e
+        * @returns {void}
+        */
+        protected void TodosGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (IsPostBack)
+            {
+                if (e.Row.RowType == DataControlRowType.Header) // if header row has been clicked
+                {
+                    LinkButton linkbutton = new LinkButton();
+
+                    for (int index = 0; index < TodosGridView.Columns.Count - 1; index++)
+                    {
+                        if (TodosGridView.Columns[index].SortExpression == Session["SortColumn"].ToString())
+                        {
+                            if (Session["SortDirection"].ToString() == "ASC")
+                            {
+                                linkbutton.Text = " <i class='fa fa-caret-up fa-lg'></i>";
+                            }
+                            else
+                            {
+                                linkbutton.Text = " <i class='fa fa-caret-down fa-lg'></i>";
+                            }
+
+                            e.Row.Cells[index].Controls.Add(linkbutton);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void PageSizeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Set the new Page size
+            TodosGridView.PageSize = Convert.ToInt32(PageSizeDropDownList.SelectedValue);
+
+            // refresh the grid
+            this.GetTodo();
+        }
     }
+
 }
